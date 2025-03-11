6b38831fe9b1916b0bb05e16ef590274c4c78edb
mthierman <mthierman@gmail.com>
2025-03-10 22:12:27 -0600

    A real example

--- examples/console/src/main.cxx
+++ examples/console/src/main.cxx
@@ -2,4 +2,8 @@
 #include <lib/lib.hxx>
 #include <print>
 
-auto main() -> int { return EXIT_SUCCESS; }
+auto main() -> int {
+    std::println("{}", lib::thirteen());
+
+    return EXIT_SUCCESS;
+}
