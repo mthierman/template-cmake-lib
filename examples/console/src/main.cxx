680288dcef0634d45a28cef750e7476ed00b79ba
mthierman <mthierman@gmail.com>
2025-03-10 22:14:18 -0600

    Use wmain

--- examples/console/src/main.cxx
+++ examples/console/src/main.cxx
@@ -2,7 +2,7 @@
 #include <lib/lib.hxx>
 #include <print>
 
-auto main() -> int {
+auto wmain([[maybe_unused]] int argc, [[maybe_unused]] wchar_t** argv) -> int {
     std::println("{}", lib::thirteen());
 
     return EXIT_SUCCESS;
