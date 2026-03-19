bb4ad7ab9f4347f6b0e58e400d9a6a0f3ca9002f
mthierman <mthierman@gmail.com>
2026-03-19 13:53:28 -0600

    Fix cmake

--- examples/example.cxx
+++ examples/example.cxx
@@ -1,5 +1,5 @@
 #include <cstdlib>
-#include <lib/lib.hxx>
+#include <lib/lib.hpp>
 #include <print>
 
 auto wmain([[maybe_unused]] int argc, [[maybe_unused]] wchar_t** argv) -> int {
--- src/lib.cxx
+++ src/lib.cxx
@@ -1,6 +1,5 @@
-#include <lib/lib.hxx>
+#include <lib/lib.hpp>
 
 namespace lib {
-//
 auto thirteen() -> int { return 13; }
 } // namespace lib
