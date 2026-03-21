cdda825ede1e7dc33282eda8aacf91e35432d4f3
mthierman <mthierman@gmail.com>
2026-03-21 17:45:28 -0600

    Fix formatter

--- include/utils/log.hpp
+++ include/utils/log.hpp
@@ -7,7 +7,7 @@ namespace utils {
 template <typename... Args>
 auto log(std::format_string<Args...> format_string, Args&&... args) -> void
 {
-    std::println(std::format(format_string, std::forward<Args>(args)...));
+    std::println(format_string, std::forward<Args>(args)...);
 }
 
 auto log(std::string_view message) -> void;
